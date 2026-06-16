114c85d6f02fadb7fb4e1a6d1f93209077dae6ca
Arnav-Gr0ver <arnavgrover@gmail.com>
2026-06-16 12:04:18 -0400

    nuked openbb too many dependency issues
    Signed-off-by: Arnav-Gr0ver <arnavgrover@gmail.com>

--- openbb_platform/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-# TODO: See if we can dynamically import the test modules under the openbb_platform
-
-# TODO: Add global fixture for headers here
-
-import os
-
-
-def pytest_configure():
-    os.environ["OPENBB_AUTO_BUILD"] = "false"
